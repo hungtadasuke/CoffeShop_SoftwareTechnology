bd0301129df6a71706b01f9849c2685dbfbe90ab
Hung <vovanhung2864@gmail.com>
2022-04-16 14:31:17 +0700

    Hung upload

---
+++ CoffeeStore/src/BUS/StaffBUS.java
@@ -0,0 +1,35 @@
+package BUS;
+
+import DAO.StaffDAO;
+import DTO.StaffDTO;
+import java.util.Vector;
+
+public class StaffBUS {
+    //attribute
+    private StaffDAO staffDAO;
+    Vector<StaffDTO> staffList;
+    
+    //constructor
+    public StaffBUS() {
+        this.staffDAO = new StaffDAO();
+        this.staffList = this.staffDAO.readStaffListFromDatabase();
+    }
+    
+    //setter and getter
+    public StaffDAO getStaffDAO() {
+        return staffDAO;
+    }
+
+    public void setStaffDAO(StaffDAO staffDAO) {
+        this.staffDAO = staffDAO;
+    }
+
+    public Vector<StaffDTO> getStaffList() {
+        return staffList;
+    }
+
+    public void setStaffList(Vector<StaffDTO> staffList) {
+        this.staffList = staffList;
+    }
+    
+}
