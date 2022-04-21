7bec5f59105c3ab0004dc35b861e1051a0df1498
ThanhLoi-300 <doanthanhloi.0003@gmail.com>
2022-04-21 16:43:15 +0700

    loi add file YEU_CAU-TRACH_NHIEM_NGHIEP_VU

--- CoffeeStore/src/GUI/FormTopping.java
+++ CoffeeStore/src/GUI/FormTopping.java
@@ -46,12 +46,12 @@ public class FormTopping extends JFrame {
         //cài đặt tọa độ, kích thước, icon, nền background cho button
         homeButton.setBounds(1230,20,50, 50);
         homeButton.setIcon(new ImageIcon("Resource\\home-icon.png"));
-<<<<<<< HEAD
+//<<<<<<< HEAD
         homeButton.setBackground(new Color(190,148,92));
         homeButton.setBorder(null);
-=======
+//=======
         homeButton.setBackground(new Color(149,231,231));
->>>>>>> hoang
+//>>>>>>> hoang
         
         saveButton.setBounds(1030, 560, 50, 50);
         saveButton.setIcon(new ImageIcon("Resource\\save-icon.png"));
